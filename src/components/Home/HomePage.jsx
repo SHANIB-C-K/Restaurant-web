e3bdf6c166e6a84e7582e719d538b4259f1aab2b
Shanib c k <ckshanib8@gmail.com>
2023-12-20 12:38:02 +0530

    images imported

--- src/components/Home/HomePage.jsx
+++ src/components/Home/HomePage.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
-import bgImg from ''
+import bgImg from '../../assets/vector3.png';
+import Food1 from '../../assets/biriyani2.png';
+import Food2 from '../../assets/biriyani3.png';
+import Food3 from '../../assets/biriyani5.png';
 
 const HomePage = () => {
   return (
