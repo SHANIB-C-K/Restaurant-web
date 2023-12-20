d5964d2ee0b58041651e74f375dd652856c9c31e
Shanib c k <ckshanib8@gmail.com>
2023-12-20 12:46:17 +0530

    homepage imported in app.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import NavBar from './components/NavBar/NavBar'
+import HomePage from './components/Home/HomePage';
 
 const App = () => {
   return (
     <>
     <NavBar />
+    <HomePage />
     </>
   )
 }
