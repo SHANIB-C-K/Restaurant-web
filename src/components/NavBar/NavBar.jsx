225c1890cfb52f56b3ed463427ca85d6c4db02ef
Shanib c k <ckshanib8@gmail.com>
2023-12-20 10:21:22 +0530

    navbar crreated in website

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Logo from "../../assets/food-logo.png";
-import {FaCartShopping} from "react-icons/fa6"
-import DarkMode from "./DarkMode";
+import { FaCartShopping } from "react-icons/fa6";
+import Darkmode from "./Darkmode";
 
 const Navbar = () => {
   return (
@@ -9,31 +9,49 @@ const Navbar = () => {
       <div className="shadow-md bg-white dark:bg-gray-900 dark:text-white duration-200">
         <div className="container py-3 sm:py-0">
           <div className="flex justify-between items-cennter">
-            <div>
-              <a href="#" className="flex items-center gap-2 text-2xl md:3xl font-bold">
+            <div className="pt-2">
+              <a
+                href="#"
+                className="flex items-center gap-2 text-2xl md:3xl font-bold"
+              >
                 <img src={Logo} alt="Foodie zone" className="w-10" />
-                Foodie
+                Restaurant
               </a>
             </div>
             <div className="flex items-center gap-4">
-                <div>
-                    <DarkMode />
-                </div>
-                <ul className="hidden sm:flex gap-4">
-                    <li>
-                        <a href="#" className="inline-block py-4 px-4 hover:text-primary">Home</a>
-                    </li>
-                    <li>
-                        <a href="#" className="inline-block py-4 px-4 hover:text-primary">About</a>
-                    </li>
-                    <li>
-                        <a href="#" className="inline-block py-4 px-4 hover:text-primary">Contact</a>
-                    </li>
-                </ul>
-                <button className="bg-gradient-to-r from-primary to-secondary text-white px-4 py-1 rounded-full hover:scale-105 duration-300 flex item-center gap-2">
-                    Order
-                    <FaCartShopping className="text-xl text-white drop-shadow-sm cursor-pointer" />
-                </button>
+              <div>
+                <Darkmode />
+              </div>
+              <ul className="hidden sm:flex gap-4">
+                <li>
+                  <a
+                    href="#"
+                    className="inline-block py-4 px-4 hover:text-primary"
+                  >
+                    Home
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="#"
+                    className="inline-block py-4 px-4 hover:text-primary"
+                  >
+                    About
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="#"
+                    className="inline-block py-4 px-4 hover:text-primary"
+                  >
+                    Contact
+                  </a>
+                </li>
+              </ul>
+              <button className="bg-gradient-to-r from-primary to-secondary text-white px-4 py-1 rounded-full hover:scale-105 duration-300 flex item-center gap-2">
+                Order
+                <FaCartShopping className="text-xl text-white drop-shadow-sm cursor-pointer" />
+              </button>
             </div>
           </div>
         </div>
