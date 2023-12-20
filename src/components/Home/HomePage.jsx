fb0e24b26efec13ceacd44628223886f5c8d145a
Shanib c k <ckshanib8@gmail.com>
2023-12-20 12:40:28 +0530

    foodlist array are created

--- src/components/Home/HomePage.jsx
+++ src/components/Home/HomePage.jsx
@@ -1,15 +1,26 @@
-import React from 'react';
-import bgImg from '../../assets/vector3.png';
-import Food1 from '../../assets/biriyani2.png';
-import Food2 from '../../assets/biriyani3.png';
-import Food3 from '../../assets/biriyani5.png';
+import React from "react";
+import bgImg from "../../assets/vector3.png";
+import Food1 from "../../assets/biriyani2.png";
+import Food2 from "../../assets/biriyani3.png";
+import Food3 from "../../assets/biriyani5.png";
+
+const ImageList = [
+  {
+    id: 1,
+    img: Food1,
+  },
+  {
+    id: 2,
+    img: Food2,
+  },
+  {
+    id: 3,
+    img: Food3,
+  },
+];
 
 const HomePage = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  return <div></div>;
+};
 
-export default HomePage
+export default HomePage;
