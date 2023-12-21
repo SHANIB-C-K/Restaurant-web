3b3a6f8e1df4a37fcc08f71c048d7afcf0c52ee2
Shanib c k <ckshanib8@gmail.com>
2023-12-21 14:10:28 +0530

    background color problem solved and card successfully added

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,23 @@
 // This is App.jsx file
 // This importing section
-import React from 'react'
-import NavBar from './components/NavBar/NavBar'
-import HomePage from './components/Home/HomePage';
-import Services from './components/Services/Services';
+import React from "react";
+import NavBar from "./components/NavBar/NavBar";
+import HomePage from "./components/Home/HomePage";
+import Services from "./components/Services/Services";
 
 const App = () => {
   return (
     <>
-    <NavBar /> 
-    {/* This is navbar page call */}
-    <HomePage />
-    {/* This is Homepage */}
-    <Services />
-    {/* This is service page  */}
+      <div className="bg-white dark:bg-gray-900 dark:text-white duration-200">
+        <NavBar />
+        {/* This is navbar page call */}
+        <HomePage />
+        {/* This is Homepage */}
+        <Services />
+        {/* This is service page  */}
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -47,16 +47,17 @@ const Services = () => {
           </div>
           {/* Card section */}
           <div>
-            <div>
+            <div className="grid grid-cols-1 sm:grid-cols-3 gap-14 md:gap-5 place-items-center">
               {ServicesData.map(({ id, img, name, description }) => {
                 return (
-                  <div key={id}>
-                    <div>
-                      <img src={img} alt="" />
+                  <div key={id} className="max-w-[300px] group rounded-2xl bg-white dark:bg-gray-800 dark:hover:bg-primary hover:text-white duration-300 shadow-xl">
+                    <div className="h-[100px]">
+                      <img src={img} alt="Biryani image" className="max-w-[200px] mx-auto block transform -translate-y-14 group-hover:scale-105 group-hover:rotate-6 duration-300"
+                      />
                     </div>
-                    <div>
-                      <h1>{name}</h1>
-                      <p>{description}</p>
+                    <div className="p-4 text-center">
+                      <h1 className="text-xl font-bold">{name}</h1>
+                      <p className="text-gray-500 group-hover:text-white duration-300 text-sm line-clamp-2">{description}</p>
                     </div>
                   </div>
                 );
