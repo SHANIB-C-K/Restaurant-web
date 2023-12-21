ebb9952e95f89475e239538554d1d3d1d6c67528
Shanib c k <ckshanib8@gmail.com>
2023-12-21 17:01:16 +0530

    bgimage variable declared

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,11 +1,27 @@
-import React from 'react';
+// This is Banner page
+
+// This is importing section
+import React from "react";
+import BiryaniImg from "../../assets/biryani5.png";
+import Vector from "../../assets/vector3.png";
+import { GrSecure } from "react-icons/gr";
+import { IoFastFood } from "react-icons/io5";
+import { GiFoodTruck } from "react-icons/gi";
 
 const Banner = () => {
+  const bgImage = {
+    backgroundImage: `url(${Vector})`,
+    backgroundPosition: "center",
+    backgroundRepeat: "no-repeat",
+    backgroundSixe: "cover",
+    height: "100%",
+    width: "100px",
+  };
   return (
-    <div>
+    <>
       
-    </div>
-  )
-}
+    </>
+  );
+};
 
-export default Banner
+export default Banner;
