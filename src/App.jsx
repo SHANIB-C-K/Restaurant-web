5130c9a6d298a5380491570ddc730f40bebde128
Shanib c k <ckshanib8@gmail.com>
2023-12-21 12:22:47 +0530

    Services page created

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import NavBar from './components/NavBar/NavBar'
 import HomePage from './components/Home/HomePage';
+import Services from './components/Services/Services';
 
 const App = () => {
   return (
     <>
     <NavBar />
     <HomePage />
+    <Services />
     </>
   )
 }
---
+++ src/components/Services/Services.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Services = () => {
+  return (
+    <>
+      Services page
+    </>
+  )
+}
+
+export default Services
