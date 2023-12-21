f88416517e4405f3ba08bcdcec1e460214a863e8
Shanib c k <ckshanib8@gmail.com>
2023-12-21 14:56:47 +0530

    formate keeped

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -50,14 +50,22 @@ const Services = () => {
             <div className="grid grid-cols-1 sm:grid-cols-3 gap-14 md:gap-5 place-items-center">
               {ServicesData.map(({ id, img, name, description }) => {
                 return (
-                  <div key={id} className="rounded-2xl bg-white dark:bg-gray-800 hover:bg-primary dark:hover:bg-primary hover:text-white relative shadow-xl duration-high group max-w-[300px]">
+                  <div
+                    key={id}
+                    className="rounded-2xl bg-white dark:bg-gray-800 hover:bg-primary dark:hover:bg-primary hover:text-white relative shadow-xl duration-high group max-w-[300px]"
+                  >
                     <div className="h-[100px]">
-                      <img src={img} alt="Biryani image" className="max-w-[200px] mx-auto block transform -translate-y-14 group-hover:scale-105 group-hover:rotate-6 duration-300"
+                      <img
+                        src={img}
+                        alt="Biryani image"
+                        className="max-w-[200px] mx-auto block transform -translate-y-14 group-hover:scale-105 group-hover:rotate-6 duration-300"
                       />
                     </div>
                     <div className="p-4 text-center">
                       <h1 className="text-xl font-bold">{name}</h1>
-                      <p className="text-gray-500 group-hover:text-white duration-300 text-sm line-clamp-2">{description}</p>
+                      <p className="text-gray-500 group-hover:text-white duration-300 text-sm line-clamp-2">
+                        {description}
+                      </p>
                     </div>
                   </div>
                 );
