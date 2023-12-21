fa3976d54dce778cea20613024099d369437abda
Shanib c k <ckshanib8@gmail.com>
2023-12-21 12:59:42 +0530

    item name and description printed successfully

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -2,28 +2,28 @@ import React from "react";
 import Img2 from "../../assets/biryani2.png";
 
 const ServicesData = [
-    {
-        id: 1,
-        img: Img2,
-        name: "Biryani",
-        description: 
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum totam vitae laudantium reprehenderit tenetur voluptatibus iste eligendi at vero quae!"
-    },
-    {
-        id: 2,
-        img: Img2,
-        name: "Chiken kari",
-        description: 
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum totam vitae laudantium reprehenderit tenetur voluptatibus iste eligendi at vero quae!"
-    },
-    {
-        id: 1,
-        img: Img2,
-        name: "Cold Cofee",
-        description: 
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum totam vitae laudantium reprehenderit tenetur voluptatibus iste eligendi at vero quae!"
-    },
-]
+  {
+    id: 1,
+    img: Img2,
+    name: "Biryani",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum totam vitae laudantium reprehenderit tenetur voluptatibus iste eligendi at vero quae!",
+  },
+  {
+    id: 2,
+    img: Img2,
+    name: "Chiken kari",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum totam vitae laudantium reprehenderit tenetur voluptatibus iste eligendi at vero quae!",
+  },
+  {
+    id: 1,
+    img: Img2,
+    name: "Cold Cofee",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum totam vitae laudantium reprehenderit tenetur voluptatibus iste eligendi at vero quae!",
+  },
+];
 
 const Services = () => {
   return (
@@ -46,17 +46,19 @@ const Services = () => {
           {/* Card section */}
           <div>
             <div>
-                {
-                    ServicesData.map(({id, img, name, description}) => {
-                        return (
-                            <div key={id}>
-                                <div>
-                                    <img src={img} alt="" />
-                                </div>
-                            </div>
-                        )
-                    })
-                }
+              {ServicesData.map(({ id, img, name, description }) => {
+                return (
+                  <div key={id}>
+                    <div>
+                      <img src={img} alt="" />
+                    </div>
+                    <div>
+                      <h1>{name}</h1>
+                      <p>{description}</p>
+                    </div>
+                  </div>
+                );
+              })}
             </div>
           </div>
         </div>
