15a2e9693dba7d1b7ded1c139f1df2bd8568da3c
SHANIB C K <ckshanib8@gmail.com>
2023-12-22 18:19:07 +0530

    slick-carousel css files imported

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,15 @@
 // This is main.jsx file
 
-// This is importing section 
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+// This is importing section
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
