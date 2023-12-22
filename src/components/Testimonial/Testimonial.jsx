4acd9fc5b66c30bcd14a5e02289b229594251be1
SHANIB C K <ckshanib8@gmail.com>
2023-12-22 18:07:25 +0530

    Header section added in Tesyimonial page

--- src/components/Testimonial/Testimonial.jsx
+++ src/components/Testimonial/Testimonial.jsx
@@ -1,11 +1,27 @@
-import React from 'react'
+import React from "react";
 
 const Testimonial = () => {
   return (
     <>
-      
+      <div>
+        <div className="container">
+          {/* Header section */}
+          <div className="text-center mb-20 max-w-[400px] mx-auto">
+            <p className="text-sm bg-clip-text text-transparent bg-gradient-to-r from-primary to-secondary">
+              Our Services
+            </p>
+            <h1 className="text-3xl font-bold">Services</h1>
+            <p className="text-xs text-gray-400">
+              {" "}
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem dolor
+              nam porro? Dolorem impedit porro magnam architecto, consequatur
+              pariatur omnis?
+            </p>
+          </div>
+        </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Testimonial
+export default Testimonial;
