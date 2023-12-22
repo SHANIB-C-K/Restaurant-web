55123f72a216ebb7e895ffba2df60ebe72751029
SHANIB C K <ckshanib8@gmail.com>
2023-12-22 11:26:26 +0530

    Heading added in AppStore.jsx file

--- src/components/AppStore/AppStore.jsx
+++ src/components/AppStore/AppStore.jsx
@@ -2,9 +2,19 @@ import React from 'react'
 
 const AppStore = () => {
   return (
-    <div>
-      
-    </div>
+    <>
+      <div className='bg-gray-100 dark:bg-gray-800 py-14'>
+        <div className='container'>
+            <div className='grid grid-cols-1 sm:grid-cols-2 items-center gap-4'>
+                <div>
+                    <h1 className='text-2xl text-center sm:text-left sm:text-4xl font-semibold dark:text-gray-400 text-gray-700'>Foodly available for Android and IOS</h1>
+                </div>
+                <div>
+                </div>
+            </div>
+        </div>
+      </div>
+    </>
   )
 }
 
