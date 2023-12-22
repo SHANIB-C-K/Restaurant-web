b0ddab2a5394e7d20d651d3f58e0eb05ca325d32
SHANIB C K <ckshanib8@gmail.com>
2023-12-22 21:27:46 +0530

    Site current status updated

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,9 +2,13 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div>
-      <h1>This site is a work in progress. Not completed</h1>
-    </div>
+    <>
+      <div className="bg-primary/20">
+        <h1 className="text-3xl sm:text-4xl font-bold justify-center text-center py-4">
+          This site is a work in progress. Not completed
+        </h1>
+      </div>
+    </>
   );
 };
 
