70e38fdcad480f83174af623f75a6e8a3f61da1f
SHANIB C K <ckshanib8@gmail.com>
2023-12-22 21:35:11 +0530

    Site status color changed

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <>
       <div className="bg-primary/20">
-        <h1 className="text-3xl sm:text-4xl font-bold justify-center text-center py-4">
+        <h1 className="text-3xl sm:text-4xl font-bold justify-center text-center py-4 text-gray-500 dark:bg-gray-800">
           This site is a work in progress. Not completed
         </h1>
       </div>
