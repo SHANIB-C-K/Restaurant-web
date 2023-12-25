edf9e2fb0c41c368dcf80637fb37db54552144dd
SHANIB C K <ckshanib8@gmail.com>
2023-12-25 10:56:30 +0530

    Food logo and Heading style added at Footer.jsx file

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -8,8 +8,12 @@ const Footer = () => {
         <div className="max-w-[1200px] mx-auto">
           <div className="grid md:grid-cols-3 py-5">
             <div className="py-4 px-4">
-              <h1>
-                <img src={FooterLogo} alt="" />
+              <h1 className="flex items-center gap-3 text-xl sm:text-3xl font-bold text-justify sm:text-left">
+                <img
+                  src={FooterLogo}
+                  alt="Footer logo"
+                  className="max-w-[50px]"
+                />
                 RESTUARENT
               </h1>
             </div>
