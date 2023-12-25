3a4addc25315b5cf8a281cac73c967889c8140b0
SHANIB C K <ckshanib8@gmail.com>
2023-12-25 11:55:26 +0530

    AOS imported at Footer.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -8,9 +8,20 @@ import Banner from "./components/Banner/Banner";
 import AppStore from "./components/AppStore/AppStore";
 import Testimonial from "./components/Testimonial/Testimonial";
 import Footer from "./components/Footer/Footer";
+import Aos from "aos";
 import "aos/dist/aos.css";
 
 const App = () => {
+  React.useEffect(() => {
+    AOS.init({
+      offset: 100,
+      duration: 500,
+      easing: "ease-in-sine",
+      delay: 100,
+    });
+    AOS.refresh();
+  }, []);
+
   return (
     <>
       <div className="bg-white dark:bg-gray-900 dark:text-white duration-200">
