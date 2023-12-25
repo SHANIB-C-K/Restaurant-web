61e01f6b81bb49fa437af4e881d97621f4b6c41b
SHANIB C K <ckshanib8@gmail.com>
2023-12-25 10:38:39 +0530

    Status gif deleted

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,19 +1,10 @@
 import React from "react";
-import Status from "../../assets/giphy.gif";
 
 const Footer = () => {
   return (
     <>
-      <div className="pt-12">
-        <div className="bg-primary/10 dark:bg-gray-900 pt-10">
-          <div data-aos="zoom-in" data-aos-duration="300">
-            <img
-              src={Status}
-              alt="Working progress image"
-              className="w-[300px] h-[300px] sm:max-w-[60%] block rounded-md mx-auto mix-blend-multiply dark:mix-blend-difference"
-            />
-          </div>
-        </div>
+      <div>
+
       </div>
     </>
   );
